b36c4b9d0087bc383c96add4c87bf14f19fda8c5
ozni <89555336+ozniGabriel@users.noreply.github.com>
2021-12-30 23:35:24 -0300

    Add menu parte 01

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMenu(){
+  document.querySelector('.menu').style = "visibility: visible"
+}
+
+function destroyMenu(){
+  document.querySelector('.menu').style = "visibility: hidden"
+}
