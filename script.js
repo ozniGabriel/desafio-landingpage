bc99731388c3e3af6eb408320f61743b119e78a5
ozni <89555336+ozniGabriel@users.noreply.github.com>
2022-01-03 19:22:41 -0300

    upgrade no menu hamburguer

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const btnExit = document.querySelector('#exit')
 const menuHamburguer = document.querySelector('.menu')
 
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 845) {
+    document.querySelector('.navigation').style = 'opacity: 1;'
+  }
+})
+
 const showMenu = () => {
-  document.querySelector('.navigation').style = 'display: flex;'
+  document.querySelector('.navigation').style =
+    'visibility: visible; opacity: 1; transition: all 0.7s;'
 }
 
 const hiddenMenu = () => {
-  document.querySelector('.navigation').style = 'visibility: hidden'
+  document.querySelector('.navigation').style =
+    'opacity: 0; transition: all 0.7s'
 }
 
 btnExit.addEventListener('click', hiddenMenu)
